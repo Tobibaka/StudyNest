484c62056b32ad0f7693c62d9491cd22f08b2d83
Pritom Kalita <101991870+Tobibaka@users.noreply.github.com>
2025-10-17 15:16:40 +0530

    Add main entry point for React application with routing setup

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import "./index.css";
+
+const rootElement = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
